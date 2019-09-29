1b3a56f59b9195843d061ea11eba671097819c3f
“Mark <mkanko66@gmail.com>
2019-09-28 18:52:48 -0700

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.querySelector("input[type=submit]");
+  input.addEventListener("click", displayTask);
 });
+
+const displayTask = (event) => { 
+  event.preventDefault();
+
+  const taskText = document.getElementById("new-task-description");
+  const ul = document.getElementById("tasks"); 
+
+  let li = document.createElement("li");
+  li.innerHTML = taskText.value; 
+  ul.appendChild(li);  
+}
